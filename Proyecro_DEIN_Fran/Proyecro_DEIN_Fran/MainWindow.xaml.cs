6c6f014e0f56ce1b56a31170c7882bf2cdc7ef22
N16K-M <n16koficial@gmail.com>
2025-10-09 20:59:58 +0200

    Creacion de proyecto y página principal

---
+++ Proyecro_DEIN_Fran/Proyecro_DEIN_Fran/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.Windows.Shapes.Path;
+
+namespace Proyecro_DEIN_Fran
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+        private void Boton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender == BotonJugar)
+            {
+                MessageBox.Show("Has clickeado Jugar");
+            }
+            else if (sender == BotonOpciones)
+            {
+                MessageBox.Show("Has clickeado Opciones");
+            }
+            else if (sender == BotonCreditos)
+            {
+                MessageBox.Show("Has clickeado Créditos");
+            }
+        }
+    }
+}
